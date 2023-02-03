30dbb86c6bd7ab573097b46634021ee081e55c0a
tanhjk <tanhjk@sph.com.sg>
2023-02-03 18:14:30 +0800

    push update for env path prefix

--- src/pages/download.js
+++ src/pages/download.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout-fixed"
-import { withPrefix } from "gatsby"
+// import { withPrefix } from "gatsby"
 
 export default function Home() {
   return (
@@ -20,7 +20,11 @@ export default function Home() {
             source Sass, JavaScript, and documentation files. This option
             requires some additional tooling:
           </p>
-          <a className="button" href={withPrefix("/") + "../../dl/luna-v1.zip"}>
+
+          <a
+            className="button"
+            href={process.env.DOMAIN_PREFIX + "../../dl/luna-v1.zip"}
+          >
             Download
           </a>
         </section>
